3e8dc19afa610a8091c26963cf5b8ce9151827c9
Yasir <ahmetyasirtug@gmail.com>
2019-07-14 05:48:37 +0300

    bug fix test

--- libloragw/tst/test_loragw_gps.c
+++ libloragw/tst/test_loragw_gps.c
@@ -205,7 +205,7 @@ int main()
         size_t frame_size = 0;
         while(i < wr_bytes)
         {
-            if(serial_buff[i] == 0xB5)
+            if(serial_buff[i] == 0xB5 && serial_buff[i + 1] == 0x62)
             {
                 printf("found\n");
                 unsigned short pl_length = serial_buff[i + 5];
